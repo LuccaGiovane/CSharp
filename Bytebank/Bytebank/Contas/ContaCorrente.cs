d3d3b2529461eb1c04afe63d8512cbeb2704c2fc
Lucca Giovane Gomes <55901179+LuccaGiovane@users.noreply.github.com>
2023-08-13 09:53:17 -0300

    Add files via upload

---
+++ Bytebank/Bytebank/Contas/ContaCorrente.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bytebank.Titular;
+
+namespace Bytebank.Contas
+{
+    internal class ContaCorrente
+    {
+        private int numero_agencia;
+        public int Numero_Agencia
+        {
+            get { return numero_agencia; }
+            set 
+            {
+                if(value > 0)
+                {
+                    this.numero_agencia = value;
+                }
+            }
+        }
+        //private string conta;
+        public string Conta { get; set; }
+        private double saldo = 100;
+        public Cliente Titular { get; set; }
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > saldo)
+            {
+                Console.WriteLine("Não foi possivel realizar o saque. Saldo insuficiente.");
+
+            }
+            else
+            {
+                saldo -= valor;
+            }
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestinataria)
+        {
+            if (valor > saldo)
+            {
+                Console.WriteLine("Saldo insuficiente. Transferencia nao realizada.");
+            }
+            else
+            {
+                saldo -= valor;
+                contaDestinataria.Depositar(valor);
+            }
+
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+    }
+}
---
+++ Bytebank/Bytebank/Program.cs
@@ -0,0 +1,60 @@
+﻿using Bytebank.Contas;
+using Bytebank.Titular;
+/*
+ContaCorrente contaDoLucca = new ContaCorrente();
+contaDoLucca.titular = "Lucca Gomes";
+contaDoLucca.numero_agencia = 123;
+contaDoLucca.conta = "1010=X";
+contaDoLucca.saldo = 2000;
+
+ContaCorrente contaDaBruna = new ContaCorrente();
+contaDaBruna.titular = "Bruna Campos";
+contaDaBruna.numero_agencia = 456;
+contaDaBruna.conta = "1010=Y";
+contaDaBruna.saldo = 1000;
+
+Console.WriteLine("Saldo da conta do Lucca " + contaDoLucca.saldo);
+Console.WriteLine("Saldo da conta da Bruna " + contaDaBruna.saldo);
+
+contaDoLucca.Transferir(200, contaDaBruna);
+
+Console.WriteLine("Saldo da conta do Lucca " + contaDoLucca.saldo);
+Console.WriteLine("Saldo da conta da Bruna " + contaDaBruna.saldo);
+
+*/
+
+/*
+Cliente cliente = new Cliente();
+
+cliente.nome = "Lucca Gomes";
+cliente.cpf = "123456789";
+cliente.profissao = "Dev";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.numero_agencia = 123;
+conta.conta = "1010=X";
+conta.saldo = 2000;
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("CPF = " + conta.titular.cpf);
+Console.WriteLine("Profissão = " + conta.titular.profissao);
+Console.WriteLine("Nº da Conta = " + conta.conta);
+Console.WriteLine("Saldo = " + conta.saldo);
+Console.WriteLine("Nº da Agência = " + conta.numero_agencia);
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = new cliente();
+conta2.numero_agencia = 123;
+conta2.conta = "1010=X";
+conta2.saldo = 2000;
+*/
+
+ContaCorrente conta3 = new ContaCorrente();
+conta3.Numero_Agencia = 18;
+conta3.Conta = "1010-Z";
+conta3.SetSaldo(1000);
+
+Console.WriteLine(conta3.GetSaldo());
+Console.WriteLine(conta3.Numero_Agencia);
+Console.WriteLine(conta3.Conta);
